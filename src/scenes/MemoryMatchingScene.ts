f8135ae1b7d6912cab293c7a158de0e1307c54da
John Bentley <jwbentle@gmail.com>
2023-10-31 20:34:48 -0700

    Cards split to their correct value

--- src/scenes/MemoryMatchingScene.ts
+++ src/scenes/MemoryMatchingScene.ts
@@ -14,18 +14,18 @@ export default class Demo extends Phaser.Scene {
       key: 'cardSheet',
       url: 'assets/card-sheet.png',
       frameConfig: {
-          frameWidth: 25,
-          frameHeight: 34,
-          startFrame: 0,
-          endFrame: 15
+        frameWidth: 60,
+        frameHeight: 84,
+        startFrame: 0,
+        endFrame: 15
       }
     });
 
   }
 
   create() {
-    const cardWidth = 35; // Adjust the size as needed
-    const cardHeight = 40;
+    const cardWidth = 60; // Adjust the size as needed
+    const cardHeight = 84;
     const gridRows = 4; // Define the number of rows and columns in the grid
     const gridCols = 13;
 
@@ -33,43 +33,31 @@ export default class Demo extends Phaser.Scene {
 
     for (let i = 0; i < gridRows; i++) {
       for (let j = 0; j < gridCols; j++) {
-        let card = this.add.sprite(j * cardWidth, i * cardHeight, 'cardSheet', j); // 'cardBack' is the key for the face-down card image
-        card.name = `Card ${j + 1}`;
+        let card = this.add.sprite(j * cardWidth, i * cardHeight, 'cardSheet', 14); // 'cardBack' is the key for the face-down card image
 
-        // Set the face cards to the right name        
-        switch (card.name) {
-          case "Card 1":
-            // Set to Ace
-            card.name = "Card Ace";
-            break;
-          case "Card 11":
-            card.name = "Card Jack"
-            break;
-          case "Card 12":
-            card.name = "Card Queen"
-            break;
-          case "Card 13":
-            card.name = "Card King"
-            break;
-          default:
-            break;
-        }
-
+        // Set a custom value attribute
+        card.value = j + 1;
+        
+        const cardValue = card.getData('value');
         cards.push(card);
       }
     }
+
 
     cards.forEach(card => {
       card.setInteractive();
       card.on('pointerdown', () => {
         // Card interaction logic
-        console.log(card.name);
+        const cardValue = card.value;
+
+        // Set the new frame of
+        // the card from the sprite sheet
+        card.setFrame(card.value);
+
+        const cardContainer = this.add.container(50, 100, cards);
+
+        this.add.existing(cardContainer);
       });
     });
-    
-
-    const cardContainer = this.add.container(200, 100, cards);
-
-    this.add.existing(cardContainer);
   }
 }
