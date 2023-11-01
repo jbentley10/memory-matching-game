06980c4e608aa3d621c08134f2a97d6e76245432
John Bentley <jwbentle@gmail.com>
2023-11-01 14:28:44 -0700

    Core game loop done!

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import Phaser from 'phaser';
 import config from './config';
 import MainMenuScene from './scenes/MainMenuScene';
 import MemoryMatchingScene from './scenes/MemoryMatchingScene';
+import './global.css';
 
 new Phaser.Game(
   Object.assign(config, {
-    scene: [MainMenuScene, MemoryMatchingScene]
+    scene: [MainMenuScene, MemoryMatchingScene],
+    fonts: [
+      { key: 'pixelFont', url: 'assets/pixelfont.ttf', fontFamily: 'PixelFont' }
+    ]
   })
 );
--- src/scenes/MemoryMatchingScene.ts
+++ src/scenes/MemoryMatchingScene.ts
@@ -9,7 +9,7 @@ export default class MemoryMatchingScene extends Phaser.Scene {
   }
 
   preload() {
-    // this.load.image('cardBack', 'assets/cardBack.png');
+    // Load the spritesheet for the cards
     this.load.spritesheet({
       key: 'cardSheet',
       url: 'assets/card-sheet.png',
@@ -21,24 +21,72 @@ export default class MemoryMatchingScene extends Phaser.Scene {
       }
     });
 
+    // Load the game background and restart button
+    this.load.image('gameBackground', 'assets/background.png');
+    this.load.image('restartButton', 'assets/restart-button.png');
   }
 
   create() {
-    const cardWidth = 60; // Adjust the size as needed
-    const cardHeight = 84;
-    const gridRows = 4; // Define the number of rows and columns in the grid
-    const gridCols = 13;
-
+    /**
+     * Initial setup
+     */
+    // Setting up the cards and grid
+    const cardWidth = 65;
+    const cardHeight = 90;
+    const gridRows = 3;
+    const gridCols = 6;
     const cards = []; // Array to store the card objects 
     
+    // Scoring and tracking player interaction
     let score = 0;  
     let scoreText;      
     let lastClickedCard = null; // Keep track of the card last clicked by the player   
     let isMatching = false; // Helps prevent further input until the match is over
 
-    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
+    // Add the background and score text
+    this.add.image(400, 300, 'gameBackground').setDisplaySize(800, 600);
+    scoreText = this.add.text(75, 60, 'Score: 0', { fontFamily: 'PixelFont', fontSize: '32px', fill: '#000' });
 
-    // Assuming you have a function to update the score display
+    // Add a restart button here
+    const restartButton = this.add.image(700, 60, 'restartButton').setScale(0.5);
+    restartButton.setInteractive();
+    restartButton.on('pointerdown', () => {
+      this.scene.restart();
+    });
+
+    /**
+     * Card interaction setup
+     */
+    
+    for (let i = 0; i < gridRows; i++) {
+      for (let j = 0; j < gridCols; j++) {
+        let card = this.add.sprite(j * cardWidth, i * cardHeight, 'cardSheet', 14); // 'cardBack' is the key for the face-down card image
+
+        // Set a custom value attribute
+        card.value = Phaser.Math.Between(0, 13);
+        card.flipped = false;
+        
+        cards.push(card);
+      }
+    }
+
+    const cardContainer = this.add.container(250, 200, cards);
+    this.add.existing(cardContainer);
+
+
+    cards.forEach(card => {
+      card.setInteractive();
+      card.on('pointerdown', () => {
+        // Only flip the card if it is not already flipped, not currently matching, and not the same as lastClickedCard
+        if (card !== lastClickedCard) {
+          flipCard(card);
+        }
+      });
+    });
+
+    /**
+     * Helper functions
+     */
     function updateScoreDisplay() {
       // Update the score display on your game UI
       // For example, if you have an HTML element with an id "scoreDisplay":
@@ -55,7 +103,7 @@ export default class MemoryMatchingScene extends Phaser.Scene {
           const lastValue = lastClickedCard.value;
           const currentValue = card.value;
     
-          if (lastValue === currentValue) {
+          if (lastClickedCard !== card && lastValue === currentValue) {
             // The cards match, keep them face-up
             // Add animations
             score++;
@@ -79,30 +127,7 @@ export default class MemoryMatchingScene extends Phaser.Scene {
           lastClickedCard = card; // Set the last clicked card to current card
         }
       }
+      console.log(card.value);
     }
-
-    for (let i = 0; i < gridRows; i++) {
-      for (let j = 0; j < gridCols; j++) {
-        let card = this.add.sprite(j * cardWidth, i * cardHeight, 'cardSheet', 14); // 'cardBack' is the key for the face-down card image
-
-        // Set a custom value attribute
-        card.value = j + 1;
-        card.flipped = false;
-        
-        cards.push(card);
-      }
-    }
-
-    const cardContainer = this.add.container(50, 100, cards);
-    this.add.existing(cardContainer);
-
-
-    cards.forEach(card => {
-      card.setInteractive();
-      card.on('pointerdown', () => {
-        // Set the users active card to the card they clicked
-        flipCard(card);
-      });
-    });
   }
 }
